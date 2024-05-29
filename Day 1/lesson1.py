dc3046705faa53d5dd130a25aa30ef95b8308554
EmiriKhazaradze <emiri.khazaradze19@gmail.com>
2024-05-29 21:48:19 +0400

    first commit

---
+++ Day 1/lesson1.py
@@ -0,0 +1,33 @@
+name = "emiri" #ეს არის str ( string ) ტიპის ცვლადი
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "emiri" არის ცვლადისთვის მნიშვნელობა
+
+surname = "khazaradze"
+# print(name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+
+print(name)
+
+age = 16 # ეს არის int ( integer ) მთელი რიცხვი 
+height = 170 #ეს არის float ტიპის ცვლადი ( ათწილადი )
+#boolean (bool) ტიპის ცვლადი
+
+knows_programming = True #True ან False
+is_ugly = False #snakecase(უბრალოდ წერის ტიპი სტანდარტულად)
+
+isUgly = False # ჯავასკრიპტული camelcase
+print(name + " " + surname)
+
+# სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები 
+# print(name + age)
+
+# print(type(age))
+# print(type(name))
+# print(type(surname))
+# print(type(height))
+# print(type(knows_programming))
+print(name + " " + str(age))
+# me var emiri chemi gvaria xazaradze var 16 wlis
+print("me var",name + " " + surname + " " + "var",str(age), "wlis"    )
