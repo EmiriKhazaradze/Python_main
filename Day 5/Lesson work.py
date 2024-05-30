c29e8fbf0871ee2c266b8a2fda8cfe27b0f74f73
EmiriKhazaradze <emiri.khazaradze19@gmail.com>
2024-05-30 16:20:26 +0400

    second commit

---
+++ Day 5/Lesson work.py
@@ -0,0 +1,8 @@
+num1 = int(input("Please enter first number: "))
+num2 = int(input("Please enter second number: "))
+num3 = int(input("Please enter third number: "))
+num4 = int(input("Please enter fourth number: "))
+
+print("Result:",num1 + num2 - num3 / num4)
+print(num1 > num2)
+print(num3 < num4)
---
+++ Day 5/Lesson5.py
@@ -0,0 +1,19 @@
+user_name = input("please enter you'r name:")
+congratulatory_message = input("Write a greeting message: ")
+father_age = int(input("plase write you'r father age:"))
+mother_age = int(input("plase write you'r mother age:"))
+brother_age = int(input("plase write you'r brother age:"))
+print(father_age + 20)
+print(mother_age + 20)
+print(brother_age + 20)
+
+
+
+
+
+user_name = input("plaease enter you'r name:")
+surname = input("plaease enter you'r surname:")
+age = input("plaease enter you'r age:")
+addres = input("plaease enter you'r addres:")
+gmail = input("plaease enter you'r gmail:")
+print(user_name + " " + surname + " " + age + " " + addres + " " + gmail)
