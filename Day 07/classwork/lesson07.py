5e0897d117fe32e3cee3de712084c3439a8d54b6
EmiriKhazaradze <emiri.khazaradze19@gmail.com>
2024-06-05 21:30:02 +0400

    commit

---
+++ Day 07/classwork/lesson07.py
@@ -0,0 +1,22 @@
+age =int(input("Please enter your age: "))
+
+print (age > 18 and age <= 25)
+
+
+
+
+
+budget =int(input("Please enter your budget: "))
+
+print (budget > 50 or budget > 100 )
+
+for i in range(10):
+  print("Emiri")
+  
+
+
+
+
+
+
+
---
+++ Day 07/homework/homework07.py
@@ -0,0 +1,53 @@
+num1 =int(input("please enter number:"))
+num2 =int(input("please enter number:"))
+num3 =int(input("please enter number:"))
+num4 =int(input("please enter number:"))
+num5 =int(input("please enter number:"))
+
+num6 = 10
+print(num6 + num1)
+print(num6 / num2)
+print(num6 - num3)
+print(num6 * num4)
+print(num6 % num5)
+print(num6 // num1)
+
+
+
+age =int(input("Please enter your age: "))
+
+print (age > 18 and age <= 20)
+
+
+num=18
+print("----------- AND -----------")
+
+print(num >= 5 and num <= 13) 
+print(num >= 6 and num <= 5) 
+print(num > 8 and num <= 10) 
+print(num > 6 and num > 12) 
+print(num == 1 and num != 3)
+print("----------- OR -----------")
+
+print(num >= 2 or num <= 13) 
+print(num >= 1 or num <= 4) 
+print(num > 6 or num <= 10) 
+print(num > 5 or num > 12) 
+print(num == 1 or num != 9)
+
+
+num7 = 15
+num8 = 15.0
+print(type(num7))
+print(type(num8))
+
+
+num9 = 10
+num10 = 10.2
+num11 = "12"
+num12 = True
+
+
+
+
+
